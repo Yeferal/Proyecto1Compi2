a0ddcc7c8af5b540c5315f31d2f271a5a6ebeb40
Yeferal <yefercuber20@gmailcom>
2020-08-11 14:04:54 -0600

    Inicio Proyecto

---
+++ src/proyecto1compi2/Main.java
@@ -0,0 +1,14 @@
+
+package proyecto1compi2;
+
+import ventanas.VentanaInicio;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        VentanaInicio ventanaInicio = new VentanaInicio();
+        ventanaInicio.setVisible(true);
+    }
+    
+}
---
+++ src/ventanas/VentanaInicio.java
@@ -0,0 +1,41 @@
+
+package ventanas;
+
+public class VentanaInicio extends javax.swing.JFrame {
+
+    
+    public VentanaInicio() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panel = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
+        panel.setLayout(panelLayout);
+        panelLayout.setHorizontalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1190, Short.MAX_VALUE)
+        );
+        panelLayout.setVerticalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 743, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(panel, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel panel;
+    // End of variables declaration//GEN-END:variables
+}
